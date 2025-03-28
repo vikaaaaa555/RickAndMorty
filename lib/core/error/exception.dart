7787f190291fa5f5017e4186f792697d1b52b5ed
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-03-28 17:58:39 +0300

    feat: Added types for errors handling

---
+++ lib/core/error/exception.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+
+class APIException extends Equatable implements Exception {
+  const APIException({
+    required this.message,
+    required this.statusCode,
+  });
+
+  final String message;
+  final int statusCode;
+
+  @override
+  List<Object> get props => [message, statusCode];
+}
+
+class UserNotRegisteredException implements Exception {
+  final String message;
+  UserNotRegisteredException(this.message);
+
+  @override
+  String toString() => message;
+}
