9bb1b828d61651fa6444809e7642c00857307bec
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-03-29 23:56:44 +0300

    feat: Created CacheException

--- lib/core/error/exception.dart
+++ lib/core/error/exception.dart
@@ -1,22 +1,16 @@
 import 'package:equatable/equatable.dart';
 
 class APIException extends Equatable implements Exception {
+  final String message;
+  final int statusCode;
+
   const APIException({
     required this.message,
     required this.statusCode,
   });
 
-  final String message;
-  final int statusCode;
-
   @override
   List<Object> get props => [message, statusCode];
 }
 
-class CacheException implements Exception {
-  final String message;
-  CacheException(this.message);
-
-  @override
-  String toString() => message;
-}
+class CacheException implements Exception {}
