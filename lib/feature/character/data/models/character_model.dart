04fbab839edfaf9d716e93888992b818771acfe6
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-03-29 13:22:00 +0300

    feat: Created CharacterModel

---
+++ lib/feature/character/data/models/character_model.dart
@@ -0,0 +1,31 @@
+import '../../../../core/common/utils/typedefs.dart';
+import '../../domain/entities/character_entity.dart';
+
+class CharacterModel extends CharacterEntity {
+  const CharacterModel({
+    required super.id,
+    required super.name,
+    required super.status,
+    required super.species,
+    required super.origin,
+    required super.image,
+  });
+
+  factory CharacterModel.fromJson(DataMap json) => CharacterModel(
+      id: json['id'],
+      name: json['name'],
+      status: json['status'],
+      species: json['species'],
+      origin: json['origin'],
+      image: json['image']
+  );
+
+  DataMap toJson() => {
+    'id': id,
+    'name': name,
+    'status': status,
+    'species': species,
+    'origin': origin,
+    'image': image
+  };
+}
