6db89e089656c9d7c0b35b1a51a9612ec91f696d
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-03-31 00:14:27 +0300

    feat: Added purpleExcellency color

--- lib/core/common/utils/constants/styles/app_colors.dart
+++ lib/core/common/utils/constants/styles/app_colors.dart
@@ -3,4 +3,5 @@ import 'dart:ui';
 class AppColors {
   static const Color blackCherry = Color(0xFF2b0f26);
   static const Color blackSabbath = Color(0xFF250320);
+  static const Color purpleExcellency = Color(0xFF9C388C);
 }
