28438ce5a750a53476102f963c7b09b66ec88097
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-03-30 22:31:58 +0300

    feat: Create AppColors & TextStyles classes

---
+++ lib/core/common/utils/constants/styles/app_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color blackCherry = Color(0xFF2b0f26);
+  static const Color blackSabbath = Color(0xFF250320);
+}
---
+++ lib/core/common/utils/constants/styles/text_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextStyles {
+  static TextStyle get white15Normal => GoogleFonts.exo2(
+      textStyle: const TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.normal,
+          color: Colors.white,
+          letterSpacing: 0.07)
+  );
+}
