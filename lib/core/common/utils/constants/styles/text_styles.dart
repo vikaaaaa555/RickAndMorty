ddfe73b3b639ade08ead98526056bcb97e62d14c
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-03-31 00:14:52 +0300

    feat: Added styles

--- lib/core/common/utils/constants/styles/text_styles.dart
+++ lib/core/common/utils/constants/styles/text_styles.dart
@@ -2,10 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class TextStyles {
-  static TextStyle get white15Normal => GoogleFonts.exo2(
+  static TextStyle get white12Normal => GoogleFonts.exo2(
       textStyle: const TextStyle(
-          fontSize: 15,
-          fontWeight: FontWeight.normal,
+          fontSize: 12,
+          fontWeight: FontWeight.w400,
+          color: Colors.white,
+          letterSpacing: 0.07)
+  );
+
+  static TextStyle get white12Medium => GoogleFonts.exo2(
+      textStyle: const TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w500,
+          color: Colors.white,
+          letterSpacing: 0.07)
+  );
+
+  static TextStyle get white16Medium => GoogleFonts.exo2(
+      textStyle: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
           color: Colors.white,
           letterSpacing: 0.07)
   );
