51b0bf906a3d0448971ccf614a00fd49e40fada2
Victoria Zviahintsava <vika.zv04@yandex.ru>
2025-04-03 21:27:15 +0300

    feat: Created dark theme

---
+++ lib/core/common/utils/constants/styles/theme_config.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+final ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  scaffoldBackgroundColor: AppColors.blackCherry,
+  appBarTheme: const AppBarTheme(
+    color: AppColors.blackCherry,
+    elevation: 0,
+    titleTextStyle: TextStyle(
+        color: Colors.white,
+        fontFamily: 'Exo2',
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.07),
+  ),
+  pageTransitionsTheme: const PageTransitionsTheme(
+    builders: <TargetPlatform, PageTransitionsBuilder>{
+      TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+    },
+  ),
+);
